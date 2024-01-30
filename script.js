94cbea0a3ae276847605a35b78eba6a89e5dc24f
Melinna <melinna.art@gmail.com>
2024-01-29 21:38:36 -0500

    add logic for making the modal active vs hidden

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const buttons = document.querySelectorAll('button')
+const closebtns = document.querySelectorAll('.close-modal')
+const overlay = document.querySelector('.overlay')
+
+buttons.forEach(button => {
+
+    button.addEventListener('click', function () {
+        const modalID = button.getAttribute('data-modal-target')
+        const modal = document.querySelector(modalID)
+        if (modal) {
+            overlay.classList.add('active')
+            modal.classList.add('active')
+        }
+    })
+})
+
+closebtns.forEach(closebtn => {
+    closebtn.addEventListener('click', () => {
+        const modal = closebtn.closest('.modal')
+        overlay.classList.remove('active')
+        modal.classList.remove('active')
+    })
+})
